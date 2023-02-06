2d354202ba13d568e9a4f818bb550b59c5d20520
oktoberlin <oktoberlin@gmail.com>
2023-02-06 21:29:14 +0700

    update index js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,6 @@
 import Head from 'next/head'
-import { useState } from 'react';
 
 export default function Home() {
-  const [questionInput, setQuestionInput] = useState("");
-  const [result, setResult] = useState();
-
-  async function onSubmit(event: any) {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ question: questionInput }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      setResult(data.result);
-      setQuestionInput("");
-    } catch (error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-    }
-  }
   
   return (
     <>
